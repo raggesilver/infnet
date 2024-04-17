4df7b7e4d5427d58ac32abbb74de0d9906fa2c0c
Paulo Queiroz <pvaqueiroz@gmail.com>
2024-04-16 22:06:02 -0300

    WEBJSI Aula 01

---
+++ README.md
@@ -0,0 +1,5 @@
+# Engenharia de Software — Infnet
+
+Projects and assignments developed during my Software Engineering major at [Infnet](https://www.infnet.edu.br).
+
+- [Programacao Web JavaScript I](./web_js_i)
---
+++ web_js_i/aulas/01/script.js
@@ -0,0 +1,26 @@
+const thisYear = new Date().getFullYear();
+
+window.onload = () => {
+  document.querySelector("#yob-input")?.setAttribute("max", thisYear);
+}
+
+function onAgeFormSubmitted(e) {
+  e.preventDefault();
+
+  const data = new FormData(e.target);
+  const yob = data.get("yob");
+  const yobNum = yob ? parseInt(yob) : null;
+  const name = data.get("name");
+
+  if (!yobNum || isNaN(yobNum) || typeof name !== "string") return;
+
+  const age = thisYear - yobNum;
+
+  alert(`${name}, sua idade é ${age}`);
+
+  const message = document.querySelector("#message");
+
+  if (message) {
+    message.innerHTML = "Você é " + (age < 100 ? "novo." : "velho.");
+  }
+}
