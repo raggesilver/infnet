208a71ee42fccfe43bfe87a5e7fddc117b87b682
Paulo Queiroz <pvaqueiroz@gmail.com>
2024-05-07 11:41:13 -0300

    [not-jquery] Show and hide elements

--- packages/not-jquery/src/index.test.ts
+++ packages/not-jquery/src/index.test.ts
@@ -16,4 +16,34 @@ describe("$", () => {
     button.el!.click();
     expect(callback).toHaveBeenCalledTimes(1);
   });
+
+  it("can modify elements' html", () => {
+    document.body.innerHTML = `<button>My button</button>`;
+    const button = $("button");
+    expect(button.el).not.toBeNull();
+    expect(button.el!.innerHTML).toBe("My button");
+    button.html("I've changed");
+    expect(button.el!.innerHTML).toBe("I've changed");
+  });
+
+  it("can hide and show elements", () => {
+    document.body.innerHTML = `<button>My button</button>`;
+    const button = $("button");
+    expect(button.el).not.toBeNull();
+    button.hide();
+    expect(button.el!.style.display).toBe("none");
+    button.show();
+    expect(button.el!.style.display).toBe("");
+  });
+
+  it("can hide and show elements without losing original display", () => {
+    const initial = "flex";
+    document.body.innerHTML = `<button style="display: ${initial}">My button</button>`;
+    const button = $("button");
+    expect(button.el).not.toBeNull();
+    button.hide();
+    expect(button.el!.style.display).toBe("none");
+    button.show();
+    expect(button.el!.style.display).toBe(initial);
+  });
 });
--- packages/not-jquery/src/index.ts
+++ packages/not-jquery/src/index.ts
@@ -1,6 +1,6 @@
 type NotNullOrUndefined<T> = Exclude<T, undefined | null>;
 
-export const $ = <E extends Element>(selector: string) => {
+export const $ = <E extends HTMLElement>(selector: string) => {
   const el = document.querySelector<E>(selector);
 
   const onn = el?.addEventListener.bind(el);
@@ -9,6 +9,28 @@ export const $ = <E extends Element>(selector: string) => {
     el,
     on(...args: Parameters<NotNullOrUndefined<typeof onn>>) {
       onn?.(...args);
+      return this;
+    },
+    html(str: string) {
+      if (el) {
+        el.innerHTML = str;
+      }
+      return this;
+    },
+    show() {
+      if (el?.style.display === "none") {
+        el.style.display = el.getAttribute("data-not-jquery-old-display") ?? "";
+        el.removeAttribute("data-not-jquery-old-display");
+      }
+      return this;
+    },
+    hide() {
+      if (el) {
+        if (el.style.display) {
+          el.setAttribute("data-not-jquery-old-display", el.style.display);
+        }
+        el.style.display = "none";
+      }
       return this;
     },
   };
