fddd29b3a562dbf5149a8317aa22eda76164a045
Paulo Queiroz <pvaqueiroz@gmail.com>
2024-05-17 00:05:03 -0300

    [pc] hw week 01 ex 01

---
+++ programming_club/homework/week_01/01_hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// https://judge.beecrowd.com/en/runs/code/39754171
+// gcc -o hello 01_hello_world.c -std=c99 -O2 -lm -Wall -Werror -Wextra
+
+int main(void) {
+  printf("Hello World!\n");
+  return 0;
+}
