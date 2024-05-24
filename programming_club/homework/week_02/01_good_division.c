68a49837805d4bcc37944a99fe4bb2b840cfd2bf
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-05-23 21:47:26 -0300

    [pc] hw week 02 ex 01

---
+++ programming_club/homework/week_02/01_good_division.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BUF_SIZE 65536
+
+// https://judge.beecrowd.com/en/runs/code/39882447
+// gcc -o division 01_good_division.c -std=c99 -O2 -lm -Wall -Werror -Wextra
+
+void buf_seek_and_readjust(char *buf, char *ptr, size_t *new_length) {
+  // If ptr is not NULL and not the beginning of the buffer
+  if (ptr && ptr != buf && *ptr != '\0') {
+    // Handle remainder
+    size_t len = strlen(ptr);
+    // Move remainder to the beginning of the buffer
+    memmove(buf, ptr, len);
+    // Read more data from stdin
+    ssize_t read_size = read(STDIN_FILENO, buf + len, BUF_SIZE - len - 1);
+    buf[len + read_size] = '\0';
+    *new_length = len + read_size;
+  } else {
+    // Read more data from stdin
+    ssize_t read_size = read(STDIN_FILENO, buf, BUF_SIZE - 1);
+    buf[read_size] = '\0';
+    *new_length = read_size;
+  }
+}
+
+int main(void) {
+  char buf[BUF_SIZE];
+  size_t len;
+
+  // Read from stdin
+  buf_seek_and_readjust(buf, NULL, &len);
+
+  char *ptr = buf;
+  float value = strtof(ptr, &ptr);
+  ptr++;
+  int members = strtol(ptr, &ptr, 10);
+  printf("%.2f\n", value / members);
+  return 0;
+}
