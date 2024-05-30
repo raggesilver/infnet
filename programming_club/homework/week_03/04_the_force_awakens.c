8564f7a07bcaa3c4dc393f0769681a13d9b1b6eb
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-05-30 08:28:27 -0300

    [pc] hw week 03 ex 04 (passes, optimized)

--- programming_club/homework/week_03/04_the_force_awakens.c
+++ programming_club/homework/week_03/04_the_force_awakens.c
@@ -4,18 +4,9 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BUF_SIZE 65536
+#define BUF_SIZE 4194304
 
-typedef struct {
-  int x;
-  int y;
-} point_t;
-
-typedef struct node_t {
-  point_t val;
-  struct node_t *next;
-} node_t;
-
+// https://judge.beecrowd.com/pt/runs/code/39977228
 // gcc-14 -o out 03_naming_roads.c -std=c99 -Wall -Werror -Wextra -O2
 
 void buf_seek_and_readjust(char *buf, char *ptr, size_t *new_length) {
@@ -37,46 +28,17 @@ void buf_seek_and_readjust(char *buf, char *ptr, size_t *new_length) {
   }
 }
 
-node_t *append_node(node_t *head, point_t val) {
-  node_t *self = malloc(sizeof(*self));
-  *self = (node_t){val, NULL};
-
-  node_t *current = head;
-  while (current) {
-    if (!current->next) {
-      break;
-    }
-    current = current->next;
-  }
-  if (current) {
-    current->next = self;
-    return head;
-  } else {
-    return self;
-  }
-}
-
-void free_nodes(node_t *head) {
-  node_t *current = head;
-  while (current) {
-    node_t *next = current->next;
-    free(current);
-    current = next;
-  }
-}
-
 int main(void) {
-  char buf[BUF_SIZE];
+  char *buf = malloc(BUF_SIZE);
   char *ptr = NULL;
   size_t len;
+  bool found = false;
 
   buf_seek_and_readjust(buf, ptr, &len);
   ptr = buf;
 
   long map_height = strtol(ptr, &ptr, 10);
   long map_width = strtol(ptr, &ptr, 10);
-
-  node_t *points_to_check = NULL;
 
   int map[map_height][map_width];
 
@@ -85,41 +47,25 @@ int main(void) {
       int val = (int)strtol(ptr, &ptr, 10);
       map[y][x] = val;
 
-      if (val == 42) {
-        points_to_check = append_node(points_to_check, (point_t){x, y});
+      if (val == 7 && x > 1 && y > 1) {
+        if (map[y - 1][x - 1] == 42 && map[y - 2][x - 2] == 7 &&
+            map[y - 2][x - 1] == 7 && map[y - 2][x] == 7 &&
+            map[y - 1][x - 2] == 7 && map[y - 1][x] == 7 &&
+            map[y][x - 2] == 7 && map[y][x - 1] == 7) {
+          found = true;
+          printf("%ld %ld\n", y, x);
+          goto after_loop;
+        }
       }
     }
-    buf_seek_and_readjust(buf, ptr, &len);
-    ptr = buf;
   }
-
-  bool found = false;
-
-  node_t *current = points_to_check;
-  while (current) {
-    point_t point = current->val;
-    int x = point.x;
-    int y = point.y;
-
-    if (x > 0 && x < map_width - 1 && y > 0 && y < map_height - 1) {
-      if (map[y - 1][x - 1] == 7 && map[y - 1][x] == 7 &&
-          map[y - 1][x + 1] == 7 && map[y][x - 1] == 7 && map[y][x + 1] == 7 &&
-          map[y + 1][x - 1] == 7 && map[y + 1][x] == 7 &&
-          map[y + 1][x + 1] == 7) {
-        printf("%d %d\n", y + 1, x + 1);
-        found = true;
-        break;
-      }
-    }
-
-    current = current->next;
-  }
+after_loop:
 
   if (!found) {
     printf("0 0\n");
   }
 
-  free_nodes(points_to_check);
+  free(buf);
 
   return 0;
 }
