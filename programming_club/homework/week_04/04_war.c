55744e39b444f7791f85dc63d2c2f22ace06b83c
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-06-20 23:50:12 -0300

    [pc] hw week 04 ex 04 — Max Verstappen edition!

--- programming_club/homework/week_04/04_war.c
+++ programming_club/homework/week_04/04_war.c
@@ -5,49 +5,13 @@
 
 #define BUF_SIZE 1024 * 1024 * 5 // 5MB
 
+// https://judge.beecrowd.com/pt/runs/code/40246877
 // gcc-14 -o out 03_bumpy_board.c -std=c99 -Wall -Werror -Wextra -O2
 
 __attribute__((always_inline)) static inline int
 cmp(const void* a, const void* b)
 {
   return (*(int*)a - *(int*)b);
-}
-
-bool battle(int** allies_ref, int* allies_count, int enemy)
-{
-  int* allies     = *allies_ref;
-  bool can_win    = false;
-  int  ally_index = -1;
-
-  for (int i = 0; i < *allies_count; i++) {
-    if (allies[i] > enemy) {
-      can_win    = true;
-      ally_index = i;
-      break;
-    }
-  }
-
-  if (can_win) {
-    // If the used ally is the first element of the array, we don't need to
-    // shift the elements. Simply increment the pointer.
-    if (ally_index == 0) {
-      *allies_ref = allies + 1;
-    }
-    // Otherwise, we need to shift the elements to the left.
-    else {
-      for (int i = ally_index; i < *allies_count - 1; i++) {
-        allies[i] = allies[i + 1];
-      }
-    }
-    (*allies_count)--;
-  } else {
-    // If we can't win, we need to remove the weakest ally. This is always the
-    // first element of the array.
-    *allies_ref = allies + 1;
-    (*allies_count)--;
-  }
-
-  return can_win;
 }
 
 int main(void)
@@ -64,20 +28,26 @@ int main(void)
   }
 
   int allies[n];
-  int allies_count = n;
   for (int i = 0; i < n; i++) {
     allies[i] = strtol(ptr, &ptr, 10);
   }
 
   // Sort the allies in ascending order
   qsort(allies, n, sizeof(int), cmp);
+  qsort(enemies, n, sizeof(int), cmp);
 
+  int* enemies_ptr = enemies;
   int* allies_ptr  = allies;
+  int* strong_ptr  = allies + n - 1;
   int  battles_won = 0;
 
-  for (int i = 0; i < n; i++) {
-    if (battle(&allies_ptr, &allies_count, enemies[i])) {
+  for (int i = n - 1; i >= 0; i--) {
+    if (*strong_ptr > enemies_ptr[i]) {
       battles_won++;
+      if (strong_ptr == allies_ptr) {
+        break;
+      }
+      strong_ptr--;
     }
   }
 
