57b14e89bbb8b26e5d87bd261c706a78bc4a5278
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-09-16 22:47:46 -0300

    [web_js_ii] TP3 10

---
+++ web_js_ii/tp3/10/script.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * Recebe dois números e retorna a soma deles.
+ *
+ * @param {number} a - O primeiro número a ser somado.
+ * @param {number} b - O segundo número a ser somado.
+ * @returns {number} A soma dos dois números.
+ */
+const soma = (a, b) => a + b;
+
+console.log(soma(5, 10));
