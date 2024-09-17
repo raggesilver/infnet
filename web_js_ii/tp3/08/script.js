32354294bb8270c6657dd2ff058f444334bdbcca
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-09-16 22:41:37 -0300

    [web_js_ii] TP3 08

---
+++ web_js_ii/tp3/08/script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * @param {string} pessoa
+ * @returns {string}
+ */
+const saudacao = (pessoa) => `Olá, ${pessoa}! Bem-vindo ao curso de JavaScript`;
+
+alert(saudacao("João"));
