3ccd0ac4892e99301e0e48313c61226eb5f05a9a
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-11-11 13:06:29 -0300

    [python_i] TP2 12

---
+++ python_i/tp2/12.py
@@ -0,0 +1,11 @@
+lst = [10, 2, 30, 4, 50, 6, 70, 8, 9, 1]
+
+high = max(lst)
+low = min(lst)
+lst_sum = sum(lst)
+avg = lst_sum / len(lst)
+
+print(f"O maior número é {high}")
+print(f"O menor número é {low}")
+print(f"A soma dos números é {lst_sum}")
+print(f"A média dos números é {avg}")
