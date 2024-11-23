1c76a45fad1735d236fba9ac4b29b6037ac7d67e
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2024-11-23 09:24:24 -0300

    [python_i] TP3 1

---
+++ python_i/tp3/01.py
@@ -0,0 +1,10 @@
+def main():
+    first_name = input("Digite seu primeiro nome: ")
+    last_name = input("Digite seu sobrenome: ")
+
+    full_name = first_name + " " + last_name
+    print("Seu nome completo é:", full_name)
+
+
+if __name__ == "__main__":
+    main()
