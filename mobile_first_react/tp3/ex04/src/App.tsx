dbf5ddbca9074d68bb7d33737f4bf9c3162e449a
Paulo Queiroz <16908491+raggesilver@users.noreply.github.com>
2025-12-12 23:05:02 -0300

    Ex 04

---
+++ mobile_first_react/tp3/ex04/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { TextoEstilizado } from "./TextoEstilizado";
+
+function App() {
+  return (
+    <main>
+      <TextoEstilizado />
+    </main>
+  );
+}
+
+export default App;
---
+++ mobile_first_react/tp3/ex04/src/TextoEstilizado.tsx
@@ -0,0 +1,14 @@
+export function TextoEstilizado() {
+  return (
+    <p
+      style={{
+        background: "#eaeaea",
+        color: "#232323",
+        padding: "1rem 0",
+        fontFamily: "'Space Grotesk', sans-serif",
+      }}
+    >
+      Este é um texto estilizado usando inline styles em React.
+    </p>
+  );
+}
